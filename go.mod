192652d8d3ba667eab90763fd0160ff11a50a5e5
Jonny Brownrigg <jonny-uk@hotmail.com>
2024-04-23 23:12:46 +0100

    adds data layer and createTaskHandler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/tasklist
+
+go 1.22.2
---
+++ tasklist.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"time"
+
+	"example.com/tasklist/internal"
+)
+
+type taskServer struct {
+	store *taskstore.TaskStore
+}
+
+func NewTaskServer() *taskServer {
+	store := taskstore.New()
+	return &taskServer{store: store}
+}
+
+func (ts *taskServer) createTaskHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling task create at %s\n", req.URL.Path)
+
+	type RequestTask struct {
+		Text string    `json:"text"`
+		Tags []string  `json:"tags"`
+		Due  time.Time `json:"due"`
+	}
+
+	type ResponseId struct {
+		Id int `json:"id"`
+	}
+
+	contentType := req.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if mediatype != "application/json" {
+		http.Error(w, "expect application/json Content-Type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+	var rt RequestTask
+	if err := dec.Decode(&rt); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := ts.store.CreateTask(rt.Text, rt.Tags, rt.Due)
+	js, err := json.Marshal(ResponseId{Id: id})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	server := NewTaskServer()
+
+	mux.HandleFunc("POST /task/", server.createTaskHandler)
+
+	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVEPORT"), mux))
+}
